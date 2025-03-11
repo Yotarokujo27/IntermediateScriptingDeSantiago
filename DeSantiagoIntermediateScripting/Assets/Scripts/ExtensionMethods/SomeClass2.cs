718f3f5d181f19b0dd692bbba9119fe6589c48bd
Alfredo De Santiago <727601@student.dusd.net>
2025-03-11 15:23:11 -0700

    almost did a script

---
+++ DeSantiagoIntermediateScripting/Assets/Scripts/ExtensionMethods/SomeClass2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SomeClass2 : MonoBehaviour
+{
+    void Start()
+    {
+        transform.ResetTransformation();
+    }
+}
